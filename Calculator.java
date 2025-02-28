abd7b9bb0c80fb19ec9357d03947419f55a8e225
AkhiNishat <nishatakhi2022@gmail.com>
2025-02-28 22:45:31 +0600

    Operator Handling & Computation

--- Calculator.java
+++ Calculator.java
@@ -1,21 +1,84 @@
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+  private class ButtonClickListener implements ActionListener {
+    private JTextField display;
 
-public class Calculator {
-    private double memory = 0;
-    private double result = 0;
-    private String currentInput = "0";
-    private String operator = ""; 
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(Calculator::new);
+    public ButtonClickListener(JTextField display) {
+        this.display = display;
     }
 
-    public Calculator() {
-        JFrame frame = new JFrame("Calculator");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 400);
-        frame.setLayout(new BorderLayout());
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+        switch (command) {
+            case "C":
+                currentInput = "0";
+                result = 0;
+                operator = "";
+                break;
+            case "CE":
+                currentInput = "0";
+                break;
+            case "=":
+                performCalculation();
+                operator = "";
+                break;
+            case "log":
+                currentInput = String.valueOf(Math.log10(Double.parseDouble(currentInput)));
+                break;
+            case "ln":
+                currentInput = String.valueOf(Math.log(Double.parseDouble(currentInput)));
+                break;
+            case "/":
+            case "*":
+            case "-":
+            case "+":
+                handleOperator(command);
+                break;
+            default:
+                if (currentInput.equals("0")) {
+                    currentInput = command;
+                } else {
+                    currentInput += command;
+                }
+                break;
+        }
+        display.setText(currentInput);
+    }
+
+    private void handleOperator(String newOperator) {
+        if (!operator.isEmpty()) {
+            performCalculation(); 
+        }
+        operator = newOperator;
+        result = Double.parseDouble(currentInput); 
+        currentInput = "0";
+    }
+
+    private void performCalculation() {
+        double input = Double.parseDouble(currentInput);
+        switch (operator) {
+            case "+":
+                result += input;
+                break;
+            case "-":
+                result -= input;
+                break;
+            case "*":
+                result *= input;
+                break;
+            case "/":
+                if (input != 0) {
+                    result /= input;
+                } else {
+                    currentInput = "Error";
+                    display.setText(currentInput);
+                    return;
+                }
+                break;
+            default:
+                break;
+        }
+        currentInput = String.valueOf(result);
+    }
+}
+}
